a09f17044c394f6f91f3ece6417ee9b7f1cc8b97
howienathan <jonathanaugta3@gmail.com>
2025-03-10 13:27:31 +0700

    da

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAa_xGMNrzkjCOmBjZnyKC7cshu3uHpDew",
+  authDomain: "pro-angkringan.firebaseapp.com",
+  projectId: "pro-angkringan",
+  storageBucket: "pro-angkringan.firebasestorage.app",
+  messagingSenderId: "953487184082",
+  appId: "1:953487184082:web:db7ac6e4d3d93e8b7b5d0b",
+  measurementId: "G-5B983D6MGT"
 };
 
 // Initialize Firebase
